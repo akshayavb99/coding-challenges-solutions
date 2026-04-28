3fddb88574980d6a3cfe353562688900009db9ce
Akshaya Balaji <37283561+akshayavb99@users.noreply.github.com>
2026-04-28 17:54:07 +0530

    Add argument and function for Step 3 (Count number of words)

--- wc-tool/main.py
+++ wc-tool/main.py
@@ -21,6 +21,8 @@ def main():
     # Add flags
     parser.add_argument("-c", "--bytes", action="store_true", help="Count bytes")
     parser.add_argument("-l", "--lines", action="store_true", help="Count number of lines")
+    parser.add_argument("-w", "--words", action="store_true", help="Count number of words")
+    parser.add_argument("-m", "--characters", action="store_true", help="Count number of characters")
     
     # Add argument for taking filename as input
     parser.add_argument("file", help="File to process")
@@ -39,6 +41,16 @@ def main():
         line_count = count_lines(args.file)
         result.append(str(line_count))
         #print(f"{line_count} {args.file}")
+    
+    if args.words:
+        word_count = count_words(args.file)
+        result.append(str(word_count))
+        #print(f"{word_count} {args.file}") 
+    
+    if args.characters:
+        char_count = count_characters(args.file)
+        result.append(str(char_count))
+        #print(f"{char_count} {args.file}")
     
     print(" ".join(result) + " " + args.file)
         
@@ -72,6 +84,32 @@ def count_lines(filepath, chunk_size=8192):
     with open(filepath, "rb") as f:
         while chunk := f.read(chunk_size):
             count += chunk.count(b"\n")
+    return count
+
+def count_words(filepath, chunk_size=8192):
+    """
+     Count the number of words in a file.
+
+     Args:
+         filepath (str): Path to the file to measure.
+
+     Returns:
+         int: Total number of words.
+     """
+    WHITESPACE = set(b" \t\n\r\v\f")
+    in_word = False
+    count = 0
+
+    with open(filepath, "rb") as f:
+        while chunk := f.read(chunk_size):
+            for byte in chunk:
+                if byte in WHITESPACE:
+                    in_word = False
+                else:
+                    if not in_word:
+                        count += 1
+                        in_word = True
+
     return count
 
 # Entry point
